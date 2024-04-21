7ec693d442a48b4ac394b6cf8405aca14c48800c
Sepideh Attar <sepideh.att@gmail.com>
2024-04-21 18:26:53 +0200

    fixed container and marketing navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,28 +1,47 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import App from './App';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'ma',
 })
-const mount = (el) => {
+const mount = (el, {onNavigate, defaultHistory}) => {
+  const history = defaultHistory || createMemoryHistory()
+  
+  if(onNavigate) {
+    history.listen(onNavigate);
+  }
+  
   ReactDOM.render(
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <Router history={history} >
+        <App />
+      </Router>
     </StylesProvider>
     ,
     el
   );
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+       
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+    },
+  };
 };
 
 if(process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if(devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory()
+    });
   }
 }
 
